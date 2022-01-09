27e7eb54821cac6abc7e2aa437c7b10b7d7eaa5e
Mustaf <mustafdiiriye@hotmail.com>
2022-01-09 17:42:15 +0200

    StackNavigation

---
+++ screens/HomeScreen.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+
+const HomeScreen = () => {
+    return (
+        <View>
+            <Text>Home Screen</Text>
+        </View>
+    )
+}
+
+export default HomeScreen
+
+const styles = StyleSheet.create({})
+
